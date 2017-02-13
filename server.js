160d8dfffcfb86335ed9610ba175babe40ff9649
MarcelloAtInfor <marcello.martins@infor.com>
2017-02-13 14:31:31 -0600

    removed ,

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var AlexaAppServer = require( 'alexa-app-server' );
 var server = new AlexaAppServer( {
     httpsEnabled: true,
     httpsPort: 443,
-    port: 80
+    port: 80,
     privateKey: 'privatekey.pem',
-    certificate: 'server.crt',
+    certificate: 'server.crt'
+
 } );
 
 server.start();
