aff18864b3372244b3463157587ead6f46aa9802
MarcelloAtInfor <marcello.martins@infor.com>
2017-02-13 14:26:08 -0600

    SSL certs

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var AlexaAppServer = require( 'alexa-app-server' );
 
 var server = new AlexaAppServer( {
     httpsEnabled: true,
+    httpsPort: 443,
+    privateKey: 'privatekey.pem',
+    certificate: 'server.crt',
     port: 80
 } );
 
