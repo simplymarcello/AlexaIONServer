5423053de1fe9f5c3853fc1517c77fe41b30d407
MarcelloAtInfor <marcello.martins@infor.com>
2017-02-13 14:29:29 -0600

    Trying to fix ssl

--- server.js
+++ server.js
@@ -4,14 +4,10 @@ var AlexaAppServer = require( 'alexa-app-server' );
 
 var server = new AlexaAppServer( {
     httpsEnabled: true,
-<<<<<<< HEAD
-    port: 443
-=======
     httpsPort: 443,
+    port: 80
     privateKey: 'privatekey.pem',
     certificate: 'server.crt',
-    port: 80
->>>>>>> aff18864b3372244b3463157587ead6f46aa9802
 } );
 
 server.start();
