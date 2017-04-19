2eff35970b79c5423db37d021db4709197bd5fe5
MarcelloAtInfor <marcello.martins@infor.com>
2017-04-19 10:56:30 -0500

    removed pre function

--- server.js
+++ server.js
@@ -17,23 +17,7 @@ var server = new AlexaAppServer( {
     httpsEnabled: true,
     httpsPort: 443,
     privateKey: "private-key.pem",
-    certificate: "certificate.pem",
-    pre: function(appServer) { 
-    	var Go = require('gonode').Go;
-		var options = {
-		    path		: 'test.go',
-		    initAtOnce	: true,	
-		}
-		var go = new Go(options, function(err) {
-		    if (err) throw err;
-			go.execute({commandText: 'GetToken'}, function(result, response) {
-			    if(result.ok) {
-			        console.log('Bearer Token: ' + response.responseText);
-			    }
-			});
-		    go.close();
-		});
-    },
+    certificate: "certificate.pem"
 } );
 
 server.start();
