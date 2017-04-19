2fb7afc723ba2262abb47f34d17c419672cd2fcc
Ubuntu <ubuntu@ip-172-31-10-43.ec2.internal>
2017-04-19 15:52:18 +0000

    working server settings

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-'use strict';
+
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
 
 var AlexaAppServer = require( 'alexa-app-server' );
 
