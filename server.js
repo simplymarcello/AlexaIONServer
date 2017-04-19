7ccd17d43c30f8eda1bb62dce81971cb3e3a41fe
MarcelloAtInfor <marcello.martins@infor.com>
2017-04-19 10:53:26 -0500

    trying to merge

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ var server = new AlexaAppServer( {
     httpsEnabled: true,
     httpsPort: 443,
     privateKey: "private-key.pem",
-    certificate: "certificate.pem"
+    certificate: "certificate.pem",
+    pre: function(appServer) { 
+    	var Go = require('gonode').Go;
+		var options = {
+		    path		: 'test.go',
+		    initAtOnce	: true,	
+		}
+		var go = new Go(options, function(err) {
+		    if (err) throw err;
+			go.execute({commandText: 'GetToken'}, function(result, response) {
+			    if(result.ok) {
+			        console.log('Bearer Token: ' + response.responseText);
+			    }
+			});
+		    go.close();
+		});
+    },
 } );
 
 server.start();
